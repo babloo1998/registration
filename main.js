a5fff70ff193e389c042aea23a3239fa07dd8e5d
Babloo Mishra <babloo.mishra@tothenew.com>
2019-10-14 12:45:39 +0530

    final commit

--- main.js
+++ main.js
@@ -1,64 +1,73 @@
-$(window).on('load', function () {
-    
-    checkCookie("username");
-    });
-
+$(window).on('load',function(){
+    if(screen.width<720){
+        if (!checkCookie("form")) {
+            setTimeout(()=>{$('#myModal').modal('show')},5000)
+            
+        }
+    } else{
+        if (!checkCookie("form")) {
+            $('#myModal').modal('show');
+        }
+    }
+});
 
 const validateForm = () => {
+    console.log("yes");
     var name = document.forms["form"]["name"].value;
     var email = document.forms["form"]["email"].value;
     var checked = document.getElementById("Check1").checked;
-    if (checked == false || email == ""){
+    if (checked == false || email == "") {
         alert("Please enter the mandatory fields");
     }
-    else{
-        checkCookie(name);
-        $('#myModal').modal('hide');
+    else {
+        setCookie();
     }
 }
 
-closeModal = () =>{
-    setCookie("username", name);
+closeModal = () => {
+    setCookie();
+}
+var delete_cookie = function (name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+function setCookie() {
+    var d = new Date();
+    d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    var x = "form=true";
+    console.log("######", x);
+    document.cookie = x;
 }
 
-function setCookie(cname,cvalue,expires) {
-    var d = new Date();
-    d.setTime(d.getTime() + (expires*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    var x = cname + "=" + cvalue + "," + expires + ",path=/";
-    console.log("######",x);
-    document.cookie = x;
-  }
-  
-  function getCookie(cname) {
+function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
-  
-  function checkCookie(sampleName) {
-    var user=getCookie("username");
-    console.log("####",user);
-    if (user) {
-      alert("Welcome again " + user);
-    } else {
-        $('#myModal').modal('show');    ``
-    //     user = sampleName;
-    //     console.log(user);
-    //    if (user != "" && user != null) {
-    //      setCookie("username", user, 30);
-    //    }
+}
+
+function checkCookie(sampleName) {
+    var user = getCookie(sampleName);
+    console.log("####", sampleName);
+    if (user === "") {
+        console.log("nooooo");
+        return false;
+
     }
-  }
+    else {
+        alert("Welcome again " + user);
+        return true;
+    }
+}
+
 
 
